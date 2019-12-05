e6562e71d0a6e97e0b401fd061e71b5dacbad0f7
league <league@imac-37.attlocal.net>
2019-12-04 19:40:15 -0800

    done

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,26 +1,37 @@
 package _03_More_Array_Fun;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[] args) {
-		
+		String[] arr = {"one ", "two ", "three ", "four ", "five "};
+		MoreArrayFun maf = new MoreArrayFun();
+		maf.norm(arr);
+		System.out.println("");
+		maf.back(arr);
+		System.out.println("");
+		maf.even(arr);
+		System.out.println("");
+		maf.rand(arr);
+		System.out.println("");
+		maf.rand(arr);
 	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	void norm(String[] arr) {
 		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
+			System.out.print(arr[i]);
 		}
 	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	void back(String[] arr) {
-		for(int i=arr.length;i>=0;i--) {
-			System.out.println(arr[i]);
+		for(int i=arr.length-1;i>=0;i--) {
+			System.out.print(arr[i]);
 		}
 	}
 	
@@ -28,7 +39,7 @@ public class MoreArrayFun {
 	void even(String[] arr) {
 		for(int i=0;i<arr.length;i++) {
 			if(i%2==0) {
-				System.out.println(arr[i]);
+				System.out.print(arr[i]);
 			}
 		}
 	}
@@ -38,8 +49,14 @@ public class MoreArrayFun {
 	void rand(String[] arr) {
 		Random gen = new Random();
 		int x;
+		ArrayList<String> lis = new ArrayList<>();
 		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
+			lis.add(arr[i]);
+		}
+		for(int i=0;i<arr.length;i++) {
+			x = gen.nextInt(lis.size());
+			System.out.print(lis.get(x));
+			lis.remove(x);
 		}
 	}
 }
